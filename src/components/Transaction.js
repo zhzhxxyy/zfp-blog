d0672c7992bf3770d92b0170b2abdc220e5215e9
zhzhxxyy <zhzhxxyy@126.com>
2021-05-09 13:52:04 +0800

    样式

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,5 +1,7 @@
 import React from "react";
-import "./css/Transaction.css";
+
+const DEFAULT_PLACEHOLDER_IMAGE =
+  "https://m.media-amazon.com/images/M/MV5BMTczNTI2ODUwOF5BMl5BanBnXkFtZTcwMTU0NTIzMw@@._V1_SX300.jpg";
 
 const Transaction = ({ tx }) => {
   return (
@@ -22,7 +24,89 @@ const Transaction = ({ tx }) => {
           </div>
         </div>
       </div>
-      <div className="jhFHoy"></div>
+      <div className="jhFHoy">
+        <div className="gVJVZs">
+          <div class="dIdVxU"></div>
+          <div className="dJDEkx">
+            <div className="cIeQia">
+              <div className="dSCEXn">
+                <div className="gMWvxK">
+                  <a href="/" class="eEewhk">
+                    17J891Bicqz5bJfhgRvwLe4QL1Cy5T7gfc
+                  </a>
+                  <div className="esnCUU">
+                    <span class="eQTRKC">0.10755960 BTC</span>
+                    <a href="/" class="eEewhk">
+                      <div>
+                        <svg
+                          viewBox="0 0 496 512"
+                          class="sc-1pmbxjh-0 ezkaTJ sc-1legmm2-0 brKiji"
+                          height="14px"
+                          selectable="1"
+                          width="14px"
+                        >
+                          <path d="M336.5 160C322 70.7 287.8 8 248 8s-74 62.7-88.5 152h177zM152 256c0 22.2 1.2 43.5 3.3 64h185.3c2.1-20.5 3.3-41.8 3.3-64s-1.2-43.5-3.3-64H155.3c-2.1 20.5-3.3 41.8-3.3 64zm324.7-96c-28.6-67.9-86.5-120.4-158-141.6 24.4 33.8 41.2 84.7 50 141.6h108zM177.2 18.4C105.8 39.6 47.8 92.1 19.3 160h108c8.7-56.9 25.5-107.8 49.9-141.6zM487.4 192H372.7c2.1 21 3.3 42.5 3.3 64s-1.2 43-3.3 64h114.6c5.5-20.5 8.6-41.8 8.6-64s-3.1-43.5-8.5-64zM120 256c0-21.5 1.2-43 3.3-64H8.6C3.2 212.5 0 233.8 0 256s3.2 43.5 8.6 64h114.6c-2-21-3.2-42.5-3.2-64zm39.5 96c14.5 89.3 48.7 152 88.5 152s74-62.7 88.5-152h-177zm159.3 141.6c71.4-21.2 129.4-73.7 158-141.6h-108c-8.8 56.9-25.6 107.8-50 141.6zM19.3 352c28.6 67.9 86.5 120.4 158 141.6-24.4-33.8-41.2-84.7-50-141.6h-108z"></path>
+                        </svg>
+                      </div>
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div className="ePxPaY"></div>
+            </div>
+          </div>
+        </div>
+        <div className="gVJVZs">
+          <div class="dIdVxU">
+            <div class="ijWhNT">
+              <svg
+                enable-background="new 0 0 32 32"
+                height="32px"
+                id="svg2"
+                version="1.1"
+                viewBox="0 0 32 32"
+                width="32px"
+                class="sc-1ub63u6-0 hDAkGl"
+              >
+                <g id="background">
+                  <rect fill="none" height="32" width="32"></rect>
+                </g>
+                <g id="arrow_x5F_full_x5F_right">
+                  <polygon points="16,2.001 16,10 2,10 2,22 16,22 16,30 30,16  "></polygon>
+                </g>
+              </svg>
+            </div>
+          </div>
+          <div className="dJDEkx">
+            <div className="emJpZF">
+              <div className="fydHVU">
+                <div className="dVzTcW">
+                  <a href="/" class="eEewhk">
+                    bc1q23fd9jr9gnj3vz3c075aadqml594k2mddw5jns
+                  </a>
+                  <div className="behEow">
+                    <span class="eQTRKC">0.10079014 BTC</span>
+                    <a href="/" class="kCGMTY eEewhk">
+                      <div>
+                        <svg
+                          viewBox="0 0 496 512"
+                          class="sc-1pmbxjh-0 ezkaTJ sc-1eth4zq-0 eKLQrs"
+                          height="14px"
+                          selectable="1"
+                          width="14px"
+                        >
+                          <path d="M336.5 160C322 70.7 287.8 8 248 8s-74 62.7-88.5 152h177zM152 256c0 22.2 1.2 43.5 3.3 64h185.3c2.1-20.5 3.3-41.8 3.3-64s-1.2-43.5-3.3-64H155.3c-2.1 20.5-3.3 41.8-3.3 64zm324.7-96c-28.6-67.9-86.5-120.4-158-141.6 24.4 33.8 41.2 84.7 50 141.6h108zM177.2 18.4C105.8 39.6 47.8 92.1 19.3 160h108c8.7-56.9 25.5-107.8 49.9-141.6zM487.4 192H372.7c2.1 21 3.3 42.5 3.3 64s-1.2 43-3.3 64h114.6c5.5-20.5 8.6-41.8 8.6-64s-3.1-43.5-8.5-64zM120 256c0-21.5 1.2-43 3.3-64H8.6C3.2 212.5 0 233.8 0 256s3.2 43.5 8.6 64h114.6c-2-21-3.2-42.5-3.2-64zm39.5 96c14.5 89.3 48.7 152 88.5 152s74-62.7 88.5-152h-177zm159.3 141.6c71.4-21.2 129.4-73.7 158-141.6h-108c-8.8 56.9-25.6 107.8-50 141.6zM19.3 352c28.6 67.9 86.5 120.4 158 141.6-24.4-33.8-41.2-84.7-50-141.6h-108z"></path>
+                        </svg>
+                      </div>
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div className="gubqVD"></div>
+            </div>
+          </div>
+        </div>
+      </div>
       <div className="jhFHoy">
         <div className="gVJVZs">
           <div className="dIdVxU">Free</div>
@@ -40,7 +124,14 @@ const Transaction = ({ tx }) => {
             </div>
           </div>
         </div>
-        <div className="gVJVZs"></div>
+        <div className="gVJVZs">
+          <div className="dIdVxU"></div>
+          <div className="dJDEkx">
+            <div className="fjudWa">
+              <span class="eQTRKC vXYMB">2020-12-22 15:09</span>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
